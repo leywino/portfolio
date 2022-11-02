b0b7696cc114d95fc1b159faf39bb42833da0233
Leywin <muhammadarshadv2@gmail.com>
2022-11-02 11:45:09 +0530

    improved formatting

--- form_validation.js
+++ form_validation.js
@@ -4,44 +4,44 @@ const email = document.getElementById("email");
 const subject = document.getElementById("subject");
 const message = document.getElementById("message");
 
-        form.addEventListener("submit", function (e) {
-        e.preventDefault();
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-        const validation = checkInput();
-        if (validation) {
-            const scriptURL =
+    const validation = checkInput();
+    if (validation) {
+        const scriptURL =
             "https://script.google.com/macros/s/AKfycbw5BKHxuTz7s1cI5glyuyWvIf-_IeDcwtWdXCq9YEZp5wxHcgW45jkDvnVc7_36DLa3Qw/exec";
 
-            fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        fetch(scriptURL, { method: "POST", body: new FormData(form) })
             .then((response) => {
                 alert("Success!", response);
 
                 const Toast = Swal.mixin({
-                toast: true,
-                position: "bottom-start",
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.addEventListener("mouseenter", Swal.stopTimer);
-                    toast.addEventListener("mouseleave", Swal.resumeTimer);
-                },
+                    toast: true,
+                    position: "bottom-start",
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener("mouseenter", Swal.stopTimer);
+                        toast.addEventListener("mouseleave", Swal.resumeTimer);
+                    },
                 });
 
                 Toast.fire({
-                icon: "success",
-                title: "Form Submitted Successfully",
+                    icon: "success",
+                    title: "Form Submitted Successfully",
                 });
 
                 setTimeout(function () {
-                location.reload();
+                    location.reload();
                 }, 3000);
             })
             .catch((error) => {
                 console.error("Error!", error.message);
             });
-        } else {
-            const Toast = Swal.mixin({
+    } else {
+        const Toast = Swal.mixin({
             toast: true,
             position: "bottom-start",
             showConfirmButton: false,
@@ -53,63 +53,63 @@ const message = document.getElementById("message");
             },
         });
 
-            Toast.fire({
+        Toast.fire({
             icon: "error",
             title: "Something Went Wrong",
-            });
-        }
-    });
+        });
+    }
+});
 
-        function checkInput() {
-        const isvalid = new Array();
-        const nameval = username.value;
-        const emailval = email.value.trim();
-        const subjectval = subject.value.trim();
-        const messageval = message.value.trim();
+function checkInput() {
+    const isvalid = new Array();
+    const nameval = username.value;
+    const emailval = email.value.trim();
+    const subjectval = subject.value.trim();
+    const messageval = message.value.trim();
 
-        if (nameval === "" || nameval.match(/^ *$/) !== null) {
-            document.getElementById("name-error").innerText =
+    if (nameval === "" || nameval.match(/^ *$/) !== null) {
+        document.getElementById("name-error").innerText =
             "Name cannot be blank.";
-            isvalid.push(false);
-        } else {
-            isvalid.push(true);
-        }
+        isvalid.push(false);
+    } else {
+        isvalid.push(true);
+    }
 
-        if (emailval === "") {
-            document.getElementById("email-error").innerText =
+    if (emailval === "") {
+        document.getElementById("email-error").innerText =
             "Email field can't be blank";
-            isvalid.push(false);
-        } else if (!isEmail(emailval)) {
-            document.getElementById("email-error").innerText =
+        isvalid.push(false);
+    } else if (!isEmail(emailval)) {
+        document.getElementById("email-error").innerText =
             "Please enter a valid email";
-            isvalid.push(false);
-        } else {
-            isvalid.push(true);
-        }
+        isvalid.push(false);
+    } else {
+        isvalid.push(true);
+    }
 
-        if (subjectval === "" || subjectval.match(/^ *$/) !== null) {
-            isvalid.push(false);
-            document.getElementById("subject-error").innerText =
+    if (subjectval === "" || subjectval.match(/^ *$/) !== null) {
+        isvalid.push(false);
+        document.getElementById("subject-error").innerText =
             "Subject field can't be empty";
-        } else {
-            isvalid.push(true);
-        }
+    } else {
+        isvalid.push(true);
+    }
 
-        if (messageval === "" || messageval.match(/^ *$/) !== null) {
-            document.getElementById("msg-error").innerText =
+    if (messageval === "" || messageval.match(/^ *$/) !== null) {
+        document.getElementById("msg-error").innerText =
             "Message field is empty";
-            isvalid.push(false);
-        } else {
-            isvalid.push(true);
-        }
-
-        if (isvalid.includes(false)) return false;
-        else return true;
+        isvalid.push(false);
+    } else {
+        isvalid.push(true);
     }
 
-    function isEmail(email) {
-        const filter =
+    if (isvalid.includes(false)) return false;
+    else return true;
+}
+
+function isEmail(email) {
+    const filter =
         /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-        if (filter.test(email)) return true;
-        else return false;
-    }
+    if (filter.test(email)) return true;
+    else return false;
+}
